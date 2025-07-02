32b13e7231750847d3dcc8fc53f6042a9cb84463
Shivani Kumari <shivani13222@gmail.com>
2025-07-02 19:00:32 +0530

    [2149] [Time Beats: 100.00%] [Memory Beats: 76.07%] - LeetPush

--- Rearrange-Array-Elements-by-Sign.java
+++ Rearrange-Array-Elements-by-Sign.java
@@ -4,7 +4,7 @@ class Solution {
         int arr[] = new int[n];
         int k = 0;
         int l = 1;
-
+// my first medium level question jisko hm bina hint k banaye
         for(int i = 0; i<n; i++){
             if(nums[i]<0 && l<n){
                 arr[l]= nums[i];
